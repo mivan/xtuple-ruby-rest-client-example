5b37e7c6e7130d7c3ed51cf8cdf985b67251dc56
Linda Nichols <lynnaloo@gmail.com>
2014-07-22 18:24:20 +0000

    More progress on Ruby client

--- restclient.rb
+++ restclient.rb
@@ -1,7 +1,10 @@
 require 'google/api_client'
 require 'signet/oauth_2/client'
 require 'dotenv'
+# This just downgrades the SSL error to a warning
+# Not a great "fix"
 require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 Dotenv.load
 
@@ -15,12 +18,10 @@ baseUrl = host + "/" + database
 
 # Initialize the client.
 client = Google::APIClient.new(
-  :application_name => ENV['APPLICATION_NAME'],
-  :application_version => ENV['APPLICATION_VERSION'],
+  :application_name => 'xtuple',
+  :application_version => 'v1alpha1',
   :port => 8443,
-  :host => ENV['HOST'],
-  :document_base => baseUrl,
-  :discovery_path => '/discovery/v1alpha1'
+  :host => ENV['HOST']
 )
 
 # Load your credentials for the service account
@@ -37,13 +38,17 @@ client.authorization = Signet::OAuth2::Client.new(
   # Request a token for our service account
   client.authorization.fetch_access_token!
 
+  # Register the discovery URL for xTuple REST
+  client.register_discovery_uri('xtuple', 'v1alpha1', baseUrl + '/discovery/v1alpha1/apis/v1alpha1/rest')
+
   # Initialize xTuple REST API. Note this will make a request to the
   # discovery service every time.
-  service = client.discovered_api('')
+  service = client.discovered_api('xtuple', 'v1alpha1')
 
   # Execute the query
-  contacts = client.execute(
-    :api_method => service[test].Contact.list(),
-    :parameters => {})
-
-  puts contacts
+  result = client.execute(
+    :api_method => service[database].Contact.get,
+    :parameters => {}
+  )
+  #
+  puts result
